d14998a3d768aed76b0955713cb5860b52cf7e3f
Jae Yoo <yooj@udel.edu>
2016-11-01 13:47:02 -0400

    Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,54 @@
+import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
+
 import java.io.*;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String args[]) {
-
-        String s = null;
+	public static void runner() {
+		String s = null;
 		String[] keys = new String[50];
 		String[] outputs = new String[50];
 
-        try {
-            
-	    // run the Unix "ps -ef" command
-            // using the Runtime exec method:
-            Process p = Runtime.getRuntime().exec("ls -al");
-            
-            BufferedReader stdInput = new BufferedReader(new 
-                 InputStreamReader(p.getInputStream()));
+		try {
 
-            BufferedReader stdError = new BufferedReader(new 
-                 InputStreamReader(p.getErrorStream()));
+			// run the Unix "ps -ef" command
+			// using the Runtime exec method:
+			Process p = Runtime.getRuntime().exec("ls -al");
 
-            // read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
-            
-            // read any errors from the attempted command
-            System.out.println("Here is the standard error of the command (if any):\n");
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
-            
-            //System.exit(0);
-        }
-        catch (IOException e) {
-            System.out.println("Exception happened - here's what I know: ");
-            e.printStackTrace();
-            System.exit(-1);
-        }
-		
+			BufferedReader stdInput = new BufferedReader(new
+					InputStreamReader(p.getInputStream()));
+
+			BufferedReader stdError = new BufferedReader(new
+					InputStreamReader(p.getErrorStream()));
+
+			// read the output from the command
+			System.out.println("Here is the standard output of the command:\n");
+			while ((s = stdInput.readLine()) != null) {
+				System.out.println(s);
+			}
+
+			// read any errors from the attempted command
+			System.out.println("Here is the standard error of the command (if any):\n");
+			while ((s = stdError.readLine()) != null) {
+				System.out.println(s);
+			}
+
+			//System.exit(0);
+		}
+		catch (IOException e) {
+			System.out.println("Exception happened - here's what I know: ");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
 		compare(keys, outputs);
-    }
-	
+	}
+
+
 	/**
 	* Compares corresponding indexes of the key and output arrays
 	* Calls the .pl script to compare key vs. output of OCR
@@ -61,6 +67,40 @@ public class Main {
 			System.out.println("Script executed successfully");
 		} 	catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	public static ArrayList<String> getAllKeys() throws FileNotFoundException {
+		// for all files in ImageKeys
+		// scan in all text to String
+		// add to arrayList and return
+		ArrayList<String> allKeys = new ArrayList<>();
+		Path dir = Paths.get("./src/ImageKeys");
+		int files = 0;
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+			for (Path file: stream) {
+				//System.out.println(file.getFileName());
+				files++;
+			}
+		} catch (IOException | DirectoryIteratorException x) {
+			// IOException can never be thrown by the iteration.
+			// In this snippet, it can only be thrown by newDirectoryStream.
+			System.err.println(x);
+		}
+		Scanner in = new Scanner(new File("./src/ImageKeys/Demolm01.txt"));
+		//Pattern grab = Pattern.compile("[a-z]");
+
+		System.out.println(in.next());
+		return allKeys;
+		//Scanner in = new Scanner(new file ());
+
+	}
+
+	public static void main(String args[]) {
+		try {
+			getAllKeys();
+		} catch (FileNotFoundException e) {
+			System.err.println("FAIL: " + e.getMessage() );
 		}
 	}
 }
