6e554c8067dbda4e41f9902be5cdd5fb3ef35d7e
Talha E <Talha E>
2016-11-07 23:20:14 +0000

    Add options

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.InputMismatchException;
 
 public class Main {
 
@@ -148,7 +149,7 @@ public class Main {
 	* @param directory containing keys, directory containing outputs
 	* (TODO): @return score array
 	*/
-	public static void analyzeOCR(String keydir, String outputdir) {
+	public static int[] runOCR(String keydir, String outputdir) {
 		try {
 			//Array of keys and outputs
 			ArrayList<String> keys;
@@ -172,11 +173,26 @@ public class Main {
 			//Indexes will match
 			for (int i = 0; i < keys.size(); i++) {
 				scores[i] = compare(keys.get(i), outputs.get(i));
-				System.out.println("Score of alignment " + i + ": " + scores[i]);
+				//System.out.println("Score of alignment " + i + ": " + scores[i]);
 			}
-			System.out.println("Average score is " + getAverage(scores));
+			//System.out.println("Average score is " + getAverage(scores));
+			return scores;
 		} catch (FileNotFoundException e) {
 			System.err.println("FAIL: " + e.getMessage() );
+			return null;
+		}
+	}
+	
+	public static void printScores(int[] array) {
+		for (int n = 0; n < array.length; n++) {
+			System.out.println("Score of image " + n + ": " + array[n]);
+		}
+	}
+	
+	public static void printOCR(String[] array) {
+		System.out.println("OCRS:");
+		for (int n = 0; n < array.length; n++) {
+			System.out.println(n + " - " + array[n]);
 		}
 	}
 
@@ -188,15 +204,88 @@ public class Main {
 	* (Done): analyzeOCR will call:
 	* 	(Done): getAllKeys() to parse data 
 	*	(Done): compare() to generate scores
-	* (TODO): Keep scores in separate data structures, allow user to print info
+	* (Done): Keep scores in separate data structures, allow user to select options
+	* (TODO): Implement each method of analysis
 	*/
 	public static void main(String args[]) {
 		//This is where the keys are stored, does not change during session
 		String keydir = "ImageKeys/";
 		String outputdir = "";
+		System.out.println("How many OCRs?");
+		Scanner s = new Scanner(System.in);
+		int numOCR = s.nextInt();
+		int[][] scores = new int[numOCR][43];
+		String[] OCRs = new String[numOCR];
+		
+		//Testing OCRs
+		int x = 0;
 		//For each OCR 
+			System.out.println("Select a name for OCR");
+			OCRs[x] = s.next();
+			//Run the OCR
 			//generate a folder of outputs
 			outputdir = "TesseractOutput/";
-			analyzeOCR(keydir, outputdir);
+			System.out.println("Testing...");
+			scores[x] = runOCR(keydir, outputdir);
+			System.out.println("Done.");
+			//x++
+			
+		//Analyzing OCRs
+		System.out.println();
+		boolean analyzing = true;
+		//Error to fix: Goes into infinite loop when letter is entered
+		while (analyzing) {
+			printOCR(OCRs);
+			System.out.println("Select an option:\n" +
+			"0 - Show scores of OCR\n" +
+			"1 - Show Average Scores\n" +
+			"2 - Compare OCRs\n" +
+			"3 - Output data as graph\n" +
+			"4 - Tabulate data by image\n" +
+			"5 - Save scores to file\n" +
+			"6 - Exit");
+			
+			//ERROR to fix: 
+			//Goes into infinite loop when letter is entered instead of number
+			int option;
+			try {
+				option = s.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Please enter a number 1-6");
+				option = 7;
+			}
+			
+			switch (option) {
+				case 0: System.out.println("Scores of "+
+						OCRs[0] + ": ");
+						printScores(scores[0]);
+						break;
+						
+				case 1: for (int n = 0; n < numOCR; n++) {
+							System.out.println("Average of "+
+							OCRs[n] + " is: " + getAverage(scores[n]));
+						}
+						break;
+						
+				case 2:
+						break;
+						
+				case 3:
+						break;
+						
+				case 4:
+						break;
+						
+				case 5:
+						break;
+						
+				case 6: analyzing = false;
+						break;
+						
+				case 7: break;
+						
+			}
+			System.out.println();
+		}
 	}
 }
